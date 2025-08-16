1e500498a11b0d93870e055a2e50ac70c19ae0e2
Augusto Honorata <contatoaugustohonorata@gmail.com>
2025-08-16 13:41:31 -0300

    feat: hello word

---
+++ bin/intro_dart.dart
@@ -0,0 +1,5 @@
+import 'package:intro_dart/intro_dart.dart' as intro_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${intro_dart.calculate()}!');
+}
---
+++ lib/hello_word.dart
@@ -0,0 +1,4 @@
+void main() {
+  print("Hello, World!");
+  print("Introdução ao Dart");
+}
